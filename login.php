d8d5f2ecfa078dcc1e298832f75f7165978a194a
mashooquealiamur <69459959+mashooquealiamur@users.noreply.github.com>
2020-08-10 15:26:41 +0500

    HTML5,CSS,JavaScript,PHP
    404 Customized page with login and contact page

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en" >
+<html>
+<head>
+  <title>Login</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="login_style.css">
+  
+  </head>
+
+<body class="body">
+	
+
+<div class="login-page">
+  <div class="form">
+
+  	<h1 style="color: white"> Please Enter Your Name & Press Submit Button  </h1>
+
+    <form action="404page.php">
+      <input type="text" name="name" placeholder="&#xf007;  Ener Your Name"/>
+      
+      <input type="submit" name="submit">
+      <p class="message"></p>
+    </form>
+
+  </div>
+</div>
+
+</body>
+</html>
+
